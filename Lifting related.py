bbc10a821faab600178cf04c69b894b5db54ecfc
KleidBukas <kleidbukas@gmail.com>
2016-09-05 00:00:07 -0600

    Collatz function

---
+++ Lifting related.py
@@ -0,0 +1,26 @@
+import random
+
+Squat = ['low bar', 'high bar', 'front squat', 'SSB', "hip squat"]
+Bench = ['bench', 'slingshot', 'legs up']
+
+select = random.randint(0,4)
+SQ = Squat[select]
+
+
+while True:
+    try:
+        data = input("Choose a lift: ")
+    except ValueError:
+        print('Wrong input.')
+        continue
+    else:
+        break
+
+if data == 'Squat':
+    print(Squat[select])
+
+elif data == 'Bench':
+    print(Bench[select])
+
+
+
